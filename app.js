a9136ba764c1ee728edbf53f9a1eb091d708728f
nolan32 <jordy.duces@hotmail.frgit config --global user.email jordy.duces@hotmail.frgit config --global user.email jordy.duces@hotmail.fr>
2017-06-06 19:01:58 +0200

    ajout de la totalité du projet final + test gh-pages

---
+++ app.js
@@ -0,0 +1,66 @@
+var count=0;
+var Alea = Math.floor(Math.random()*situations.length);
+
+function GenererPreventionAlea(){
+	$('#img').append("<img class='responsive-img' src='res/"+situations[Alea].genre+"'>");
+	$('#situation').html(situations[Alea].situation);
+	$('#resA').html(situations[Alea].resA);
+	$('#resB').html(situations[Alea].resB);
+}
+
+function CreateButon(){
+	$('#CreateButon').append("<button class='btn waves-effect green darken-2 btn_redirect' id='redirection'>En savoir plus...</button>");
+	$('#CreateButon').delegate('#redirection','click',function(){
+		window.location.href = situations[Alea].redirect;
+	});
+}
+
+function reponsePreventionMessage(){
+	if ($("#radioBtn input").is(':checked')){
+		var test = $("#radioBtn input:checked").val();
+		console.log(test);
+
+		if(test === situations[Alea].response){	
+			if (situations[Alea].response === "resA") {		
+				$("#reponsePrevention").html('<div class="card-panel light-green lighten-2 Panel">'+situations[Alea].ifResA+'</div>');
+				if (count===0) {
+					setTimeout(CreateButon,7000);
+				}
+			}else{
+				$("#reponsePrevention").html('<div class="card-panel light-green lighten-2 Panel">'+situations[Alea].ifResB+'</div>');
+				if (count===0) {
+					setTimeout(CreateButon,7000);
+				}
+			}
+		}else{
+			if (situations[Alea].response === "resB") {
+				$('#reponsePrevention').html('<div class="card-panel red lighten-2 Panel">'+situations[Alea].ifResA+'</div>');
+				if (count===0) {
+					setTimeout(CreateButon,7000);
+				}
+			}else{
+				$('#reponsePrevention').html('<div class="card-panel red lighten-2 Panel">'+situations[Alea].ifResB+'</div>');
+				if (count===0) {
+					setTimeout(CreateButon,7000);
+				}
+			}
+		}
+	}
+}
+
+$(document).ready(function(){
+	GenererPreventionAlea();
+	$("#radioBtn input").click(function() {
+		reponsePreventionMessage();
+		count ++;
+	});
+
+	$('.carousel').carousel();
+	$('.carousel').carousel('next');
+	$('.carousel').carousel('next', 3); // Move next n times.
+	// Previous slide
+	$('.carousel').carousel('prev');
+	$('.carousel').carousel('prev', 4); // Move prev n times.
+	$('.carousel').carousel('set', 4);
+});
+
